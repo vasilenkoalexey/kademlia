5d36f8eb5ce5d0054e24e461fc23210dbec893bd
David Keller <david.keller@litchis.fr>
2019-02-17 00:14:32 +0100

    Coverage scripts no longer create the build dir
    They expect to be run from the build directory.
    cmake command is invoked to build the project if
    not already configured & build.
    
    Signed-off-by: David Keller <david.keller@litchis.fr>

--- scripts/linux_coverage.sh
+++ scripts/linux_coverage.sh
@@ -5,10 +5,10 @@ set -e
 binary_dir=$(pwd)
 source_dir=$(cd $(dirname $0)/..; pwd)
 
-mkdir -p build-coverage && cd build-coverage
 CXXFLAGS="-fprofile-arcs -ftest-coverage" cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 lcov --quiet --output-file app_base.info --initial --capture --directory .
-cmake --build . --target unit_tests
+cmake --build . -- -j $(sysctl -n hw.ncpu)
+cmake --build . --target test -- -j $(sysctl -n hw.ncpu)
 lcov --quiet --output-file app_test.info --capture --directory .
 lcov --quiet --output-file app_total.info --add-tracefile app_base.info --add-tracefile app_test.info
 lcov --quiet --output-file app_total_stripped.info --extract app_total.info "*/include/kademlia/*" "*/src/kademlia/*"
--- scripts/osx_coverage.sh
+++ scripts/osx_coverage.sh
@@ -3,24 +3,19 @@
 set -e
 
 binary_dir=$(pwd)
-source_dir=$(dirname $0)/..
+source_dir=$(cd $(dirname $0)/..; pwd)
 test_dir=test/unit_tests
 
-# Convert 
-(
-    cd $binary_dir/$test_dir
-    xcrun llvm-profdata merge -output default.prof default.profraw
-)
-
-# Generate report
-(
-    cd $source_dir/$project_dir
-    xcrun llvm-cov show \
-        -instr-profile $binary_dir/$test_dir/default.prof \
-        -Xdemangler xcrun -Xdemangler c++filt \
-        -format html \
-        -show-line-counts-or-regions \
-        -output-dir $binary_dir/$test_dir/report \
-        $binary_dir/$test_dir/kademlia-test \
-        $(find src include -name '*.cpp' -or -name '*.hpp')
-)
+CXXFLAGS="-fprofile-instr-generate -fcoverage-mapping" cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ${source_dir}
+cmake --build . -- -j $(sysctl -n hw.ncpu)
+cmake --build . --target test -- -j $(sysctl -n hw.ncpu) 
+xcrun llvm-profdata merge -output default.prof ${test_dir}/default.profraw
+cd $source_dir
+xcrun llvm-cov show \
+    -instr-profile ${binary_dir}/default.prof \
+    -Xdemangler xcrun -Xdemangler c++filt \
+    -format html \
+    -show-line-counts-or-regions \
+    -output-dir $binary_dir/html \
+    $binary_dir/$test_dir/kademlia-test \
+    $(find src/kademlia include/kademlia -name '*.cpp' -or -name '*.hpp')
